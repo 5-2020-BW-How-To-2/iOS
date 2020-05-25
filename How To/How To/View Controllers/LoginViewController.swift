3d36e6390f442a0c056281a72a456c4bae3c7c2b
Chris Dobek <dobek.christopher@gmail.com>
2020-05-25 12:22:22 -0400

    LoginVC Outlets and Actions Made

--- How To/How To/View Controllers/LoginViewController.swift
+++ How To/How To/View Controllers/LoginViewController.swift
@@ -8,23 +8,44 @@
 
 import UIKit
 
+enum LoginType: String {
+    case signIn = "Sign In"
+    case signUp = "Sign Up"
+}
+
 class LoginViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var loginTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    // MARK: - Properties
+     var loginType: LoginType?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        submitButton.backgroundColor = UIColor(hue: 190/360, saturation: 70/100, brightness: 80/100, alpha: 1.0)
+        loginType = .signIn
+        submitButton.tintColor = .white
+        submitButton.layer.cornerRadius = 8.0
+        submitButton.setTitle("Sign In", for: .normal)
+    }
+    
+    @IBAction func loginTypeChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 1 {
+            loginType = .signUp
+            submitButton.setTitle("Sign Up", for: .normal)
+        } else {
+            loginType = .signIn
+            submitButton.setTitle("Sign In", for: .normal)
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        
     }
-    */
 
 }
